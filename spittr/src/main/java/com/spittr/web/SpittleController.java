4d7cbb6a491c9d153896a8036c04ea2078cc96e8
Vinayak More <vinayak.more@infibeam.net>
2017-04-04 10:55:09 +0530

    spittle test complete

--- spittr/src/main/java/com/spittr/web/SpittleController.java
+++ spittr/src/main/java/com/spittr/web/SpittleController.java
@@ -1,10 +1,29 @@
 package com.spittr.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.spittr.web.repository.SpittleRepository;
 
 @Controller
 @RequestMapping("/spittles")
 public class SpittleController {
+	
+	private SpittleRepository repository;
+
+	@Autowired
+	public SpittleController(SpittleRepository repository){
+		this.repository = repository;
+		
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String spittles(Model model){
+		model.addAttribute(repository.findSpittles(Long.MAX_VALUE, 20));
+		return "spittles";
+	}
 
 }
--- spittr/src/test/java/spittr/SpittrTest.java
+++ spittr/src/test/java/spittr/SpittrTest.java
@@ -4,13 +4,17 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.InternalResourceView;
 
 import com.spittr.web.HomeController;
+import com.spittr.web.SpittleController;
 import com.spittr.web.bean.Spittle;
 import com.spittr.web.repository.SpittleRepository;
 
@@ -22,13 +26,31 @@ public class SpittrTest {
 		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 		mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(
 				MockMvcResultMatchers.view().name("home"));
-
 	}
 
 	@Test
-	public void testSpittleController() {
+	public void testSpittleController() throws Exception {
 		List<Spittle> expectedSpittles = createSpittleList(20);
-		SpittleRepository spittleRepository;
+		SpittleRepository spittleRepository = Mockito
+				.mock(SpittleRepository.class);
+		Mockito.when(spittleRepository.findSpittles(Long.MAX_VALUE, 20))
+				.thenReturn(expectedSpittles);
+
+		SpittleController controller = new SpittleController(spittleRepository);
+		MockMvc mockMvc = MockMvcBuilders
+				.standaloneSetup(controller)
+				.setSingleView(
+						new InternalResourceView("/WEB-INF/views/spittles.jsp"))
+				.build();
+		mockMvc.perform(MockMvcRequestBuilders.get("/spittles"))
+				.andExpect(MockMvcResultMatchers.view().name("spittles"))
+				.andExpect(
+						MockMvcResultMatchers.model().attributeExists(
+								"spittleList"))
+				.andExpect(
+						MockMvcResultMatchers.model().attribute("spittleList",
+								Matchers.hasItems(expectedSpittles.toArray())));
+
 	}
 
 	private List<Spittle> createSpittleList(int count) {
