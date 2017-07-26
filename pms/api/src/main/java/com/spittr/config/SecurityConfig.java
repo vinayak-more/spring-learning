d983756049691db2c8829b43db0207fa7be5aa02
Vinayak More <vinayak.more@infibeam.net>
2017-07-26 19:52:40 +0530

    Spring security impl started

---
+++ pms/api/src/main/java/com/spittr/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.spittr.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Vinayak More
+ *
+ * @date 26-Jul-2017
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
+
+
+
+}
---
+++ pms/api/src/main/java/com/spittr/config/SecurityWebInitializer.java
@@ -0,0 +1,13 @@
+package com.spittr.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * @author Vinayak More
+ *
+ * @date 26-Jul-2017
+ */
+public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer{
+
+}
+
